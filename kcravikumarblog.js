8f209a724ead067c6541a25d559c65fe335702bb
Ravikumar <23378988+kcravikumar@users.noreply.github.com>
2019-02-14 18:35:53 +0530

    Update kcravikumarblog.js

--- kcravikumarblog.js
+++ kcravikumarblog.js
@@ -1,3 +1,15 @@
-function hello1{
-  alert(11) ;
+function myFunction(id) {
+var children = $(id).children().children();
+
+for (var i = 5; i < children.length; i++) {
+    var currentChild = children.eq(i);
+
+	if (currentChild.css('display') == 'none' )	 {
+		currentChild.css('display', 'block');
+		console.log('block ' + i) ;
+	} else {
+		currentChild.css('display', 'none');
+		console.log('none ' + i) ;
+	}
+ }
 }
