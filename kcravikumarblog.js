db29e60cfdbaab0616ba598c28babeeef7fd77b1
Ravikumar <23378988+kcravikumar@users.noreply.github.com>
2019-02-14 15:22:14 +0530

    Update kcravikumarblog.js

--- kcravikumarblog.js
+++ kcravikumarblog.js
@@ -1,14 +1,3 @@
 function hello1{
   alert(11) ;
-
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-
-
-alert(22) ;
 }
